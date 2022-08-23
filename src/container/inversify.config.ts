dbc4fceea9b2489331b89151a91b7d20262c4a7f
Alexsander Hage de Melo <hagemelo@gmail.com>
2022-08-23 19:29:31 -0300

    feat: criado data_source_pool

--- src/container/inversify.config.ts
+++ src/container/inversify.config.ts
@@ -1,10 +1,21 @@
+import { Pool } from 'pg';
 import { Container } from "inversify";
 import { EventSource } from "../domain";
 import { EventSourceImpl } from "../resources/event-source-impl";
 import TYPES from "./types";
 
-
 const container: Container = new Container();
+
+container.bind<Pool>(TYPES.DataSourcePool).toDynamicValue(async () => {
+    const data_source_pool = new Pool({
+        user: process.env.DB_USER,
+        host: process.env.DB_HOST,
+        database: process.env.DB_NAME,
+        password: process.env.DB_PASSWORD,
+        port: process.env.DB_PORT,
+      });
+    return data_source_pool;
+});
 
 container.bind<EventSource>(TYPES.EventSource).to(EventSourceImpl);
 
--- src/container/types.ts
+++ src/container/types.ts
@@ -1,5 +1,6 @@
 export const TYPES = {
     EventSource: Symbol('EventSource'),
+    DataSourcePool: Symbol('DataSourcePool'),
 
   };
   
