1002f75b84446bd667f8696b734abbb504a44afe
Alexsander Hage de Melo <hagemelo@gmail.com>
2023-01-24 11:40:10 -0300

    Ajuste para typeScript

---
+++ src/domain/verify-security.ts
@@ -0,0 +1,12 @@
+
+
+export interface VerifySecurity {
+
+
+
+    validarTokenExpirado
+
+    existHeadertkuuid
+const execVerify = event => {
+    authenticate(params: AuthenticateParams): Promise<Boolean>;
+}
